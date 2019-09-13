af998d9deb34ed3c9d60adf34ca9a7de790cb11b
Shivaraman Aiyer <sra392@gmail.com>
2019-09-13 23:05:42 +0530

    Lifted the state of Squares up to the Board component and handling click here as well

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-constructor(props){
-  super(props);
-  this.state = {
-    value : null,
-  }
-}
-
   render() {
     return (
-      <button className="square" onClick={() =>  alert('click') }>
+      <button 
+        className="square" 
+        onClick={() =>  this.props.onClick() }
+      >
         {this.props.value}
       </button>
     );
@@ -20,8 +16,28 @@ constructor(props){
 }
 
 class Board extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+          <Square 
+              value={this.state.squares[i]}
+              onClick={() => this.handleClick(i)}
+            />
+    );
   }
 
   render() {
