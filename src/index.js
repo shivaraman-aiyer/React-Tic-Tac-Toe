b7a819239aaf77babf590fc53bbc3fd54a60f410
Shivaraman Aiyer <sra392@gmail.com>
2019-10-04 18:55:30 +0530

    Some beautification

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 function Square(props) {
     return (
       <button 
-        className="square" 
+        className={"square value-" + props.value}
         onClick={props.onClick}
       >
         {props.value}
